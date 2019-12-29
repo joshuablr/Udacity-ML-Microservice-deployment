6cd9ce671c4586967b5d775a6d37f817da9ab0a1
jjebaraj <josuva.jebaraj@harman.com>
2019-12-30 00:29:59 +0530

    Feat: Added support for kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=joshuablr/udacity-housing-prediction:v1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-prediction --port=80 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $(kubectl get pods -o=name | grep udacity-prediction | sed "s/^.\{4\}//") 8000:80
